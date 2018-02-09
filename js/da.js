e8c3eea05a0c7177fc398055ad5fbefbf5ede2ce
Park June Chul <pjc0247@naver.com>
2018-02-09 19:56:05 +0900

    Main ICO 날짜 변경

--- js/da.js
+++ js/da.js
@@ -11,8 +11,8 @@ var today = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 
 // PRE ICO 시작시간
 // 이 시간이 되면 화면이 ico 시작화면으로 변경됨.
 //var preicoStartDate = new Date("January 25, 2018 5:17");
-var preicoStartDate = new Date("February 19, 2018 13:00");
-var buyTokenButtonDate = new Date("February 19, 2018 12:00");
+var preicoStartDate = new Date("March 12, 2018 13:00");
+var buyTokenButtonDate = new Date("March 12, 2018 12:00");
 
 // PRE ICO 끝 날짜
 // 이벤트 없음
