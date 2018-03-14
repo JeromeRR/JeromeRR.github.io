dcf4549cfd7ed0fd0dd5a2135f2a5e417b428cb4
Nam Hyeon Uk <jwvg0425@naver.com>
2018-03-14 13:31:20 +0900

    UMaster: New design (reverted from commit b0b7d20163134ca63e9b903c5fd6f526e329d64d)

--- js/teams-scroll.js
+++
@@ -1,9 +0,0 @@
-setTimeout(function () {
-  $('#scroll-wrap').perfectScrollbar({
-    suppressScrollX: true
-  });
-}, 300);
-
-$(window).on('resize', function () {
-  $('#scroll-wrap').perfectScrollbar('update');
-});
