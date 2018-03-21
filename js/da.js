1c8f7f3167c7e0fa7ef2e5fce4b46b25d7a79b72
YD <yd@realityreflection.com>
2018-03-21 15:31:19 +0900

    무언가 현욱님이 바꿈

--- js/da.js
+++ js/da.js
@@ -26,15 +26,16 @@ var addr = '0x3B3D913C2DE8Aa5be94A40BF73d044233861c38b';
 // QR코드 이미지 1:1 비율
 var qr = 'img/qrcode.png';
 
-// Hardcap
-var ico_hardcap = 25000000.00;
-
 // 공지사항을 나타낼것인지
 var view_notice = true;
 
 // 지갑데이터 수신 받기 전 초기 값.
 var ico_current = 0;
 
+var presaled = 66948856;
+
+// Hardcap
+var ico_hardcap = presaled + 123750302.00;
 
 /*
  Load ETH
@@ -74,11 +75,11 @@ function loadeth(){
         }
     };
 
-    var token_addr = '0x86789b2DE83B9A93F89F8C2Cb14d622CD73515e9';
+    var token_addr = '0x1716Ee24Abc334A6e1487A78987B54bB439f028A';
     saled(token_addr, function(err, result)
     {
         result.e -= 18;
-        ico_current = Math.max(0, result.toNumber() - 41949386.94);
+        ico_current = Math.max(0, result.toNumber());
         icoprogress();
     });
 
@@ -156,8 +157,6 @@ function icoprogress(){
     //    },2000
     //);
 
-    $("#hardcap-value").text(addComma(ico_hardcap)+".00");
-
     $("#progressbar").css({
         width : percent+'%'
     });
@@ -189,8 +188,11 @@ $(function(){
     $(".whitepaper-part-2 > .back").on("click",function(){
         $('.whitepaper-part-1, .whitepaper-part-2, .whitepaper-download-area').removeClass('active');
         return false;
-
     })
+
+    $('.progressbar-under').css({
+        width : (presaled / ico_hardcap) * 100 + '%'
+    });
 
     
     if(newNotice) {
@@ -281,31 +283,7 @@ $(function(){
         $("#product-advisor").removeClass("product-limit");
     });
 
-    //Pre ICO start 화면 변경
-    var diffdate = today-preicoStartDate;
-    var remaining = diffdate/1000;
-
-    var buttondate = today - buyTokenButtonDate;
-    var buttonremaining = buttondate/1000;
-
-    if(remaining < 0){
-        $("#upcomming").addClass("active");
-        $("#preico").removeClass("active");
-        
-        if(buttonremaining < 0) {
-            $("#buytoken-button").removeClass("active");
-            $("#whitelist-button").addClass("active");
-        }
-        else{
-            $("#buytoken-button").addClass("active");
-            $("#whitelist-button").removeClass("active");
-        }
-    } else {
-        $("#upcomming").removeClass("active");
-        $("#preico").addClass("active");
-        loadeth();
-    }
-
+    loadeth();
 });
 
 $(document).ready(function() {
@@ -324,3 +302,4 @@ $(document).ready(function() {
         $('.tip').removeClass('is-active');
     });
 });
+
