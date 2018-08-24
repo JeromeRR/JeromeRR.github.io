abe3adb0b0391622e317b8c6d08cb695adb8c799
Jerome <hernandez.jrme@yahoo.com>
2018-08-24 16:23:34 +0900

    Everypage animated

---
+++ js/revealscroll.js
@@ -0,0 +1,91 @@
+ScrollReveal().reveal('.scaleup', { 
+          scale: 0.85,
+          delay: 700,
+          duration: 1500});
+
+ScrollReveal().reveal('.game', { 
+          distance: '100px',
+          origin: 'bottom',
+          duration: 1200});
+
+         ScrollReveal().reveal('h2', { 
+          distance: '100px',
+          origin: 'bottom',
+          easing: 'ease-out',
+          opacity: 0,
+          duration: 1500});
+
+         ScrollReveal().reveal('.info', { 
+          distance: '100px',
+          origin: 'bottom',
+          easing: 'ease-out',
+          delay: 500,
+          opacity: 0,
+          duration: 1200});
+
+         ScrollReveal().reveal('.pic1', { 
+          distance: '100px',
+          origin: 'left',
+          easing: 'ease-out',
+          opacity: 0,
+          delay: 500,
+          duration: 1200});
+
+         ScrollReveal().reveal('.pic2', { 
+          distance: '100px',
+          origin: 'right',
+          easing: 'ease-out',
+          opacity: 0,
+          interval: 600,
+          delay: 500,
+          duration: 1200});
+
+         ScrollReveal().reveal('.pic345', { 
+          distance: '100px',
+          origin: 'bottom',
+          easing: 'ease-out',
+          interval: 600,
+          delay: 500,
+          opacity: 0,
+          duration: 1200});
+
+         ScrollReveal().reveal('.partners', { 
+          easing: 'ease-out',
+          interval: 200,
+          delay: 500,
+          opacity: 0,
+          duration: 1200});
+
+         ScrollReveal().reveal('.footer-left', { 
+          distance: '100px',
+          origin: 'left',
+          easing: 'ease-out',
+          opacity: 0,
+          delay: 500,
+          interval: 400,
+          duration: 1200});
+
+         ScrollReveal().reveal('.footer-right', { 
+          distance: '100px',
+          origin: 'right',
+          easing: 'ease-out',
+          opacity: 0,
+          delay: 500,
+          duration: 2400});
+
+         ScrollReveal().reveal('.video', { 
+          distance: '100px',
+          origin: 'bottom',
+          easing: 'ease-out',
+          delay: 800,
+          opacity: 0,
+          duration: 1200});
+
+         ScrollReveal().reveal('.pic-slider', { 
+          distance: '100px',
+          origin: 'right',
+          easing: 'ease-out',
+          opacity: 0,
+          interval: 600,
+          delay: 1200,
+          duration: 1200});
