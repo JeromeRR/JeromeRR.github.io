517b5e689d640e8ffd0028ea9beed275692fcf67
Jerome <hernandez.jrme@yahoo.com>
2018-12-11 14:23:48 +0900

    update

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-var animData = {
-    container: document.getElementById('bm'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'crown-animation.json'
-};
+// var animData = {
+//     container: document.getElementById('bm'),
+//     renderer: 'svg',
+//     loop: true,
+//     autoplay: true,
+//     path: 'crown-animation.json'
+// };
 
-var anim2Data = {
-    container: document.getElementById('bm2'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'ufo-animation.json'
-};
+// var anim2Data = {
+//     container: document.getElementById('bm2'),
+//     renderer: 'svg',
+//     loop: true,
+//     autoplay: true,
+//     path: 'ufo-animation.json'
+// };
 
-var anim3Data = {
-  container: document.getElementById('bm4'),
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: 'pacman-animation.json'
-};
+// var anim3Data = {
+//   container: document.getElementById('bm4'),
+//   renderer: 'svg',
+//   loop: true,
+//   autoplay: true,
+//   path: 'pacman-animation.json'
+// };
 
-var anim4Data = {
-  container: document.getElementById('bm5'),
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: 'rocket-animation.json'
-};
+// var anim4Data = {
+//   container: document.getElementById('bm5'),
+//   renderer: 'svg',
+//   loop: true,
+//   autoplay: true,
+//   path: 'rocket-animation.json'
+// };
 // var anim3Data = {
 //     container: document.getElementById('bm3').addEventListener('mouseover', function(){ anim.play(); }),
 //     renderer: 'svg',
@@ -48,33 +48,95 @@ var anim4Data = {
 
 
 
-var anim = bodymovin.loadAnimation(animData);
-var anim2 = bodymovin.loadAnimation(anim2Data);
-var anim3 = bodymovin.loadAnimation(anim3Data);
-var anim4 = bodymovin.loadAnimation(anim4Data);
+var anim = document.getElementById("bm");
+var anim2 = document.getElementById("bm2");
+var anim3 = document.getElementById("bm4");
+var anim4 = document.getElementById("bm5");
 var loader1 = document.getElementById("bm3");
 
 
 function loadOpenAnimation(loader) {
+  
+  if(loader == loader1){
   var animationIn = bodymovin.loadAnimation({
     container: loader,
     renderer: "svg",
     loop: false,
     autoplay: false,
-    path: 'folder-opening.json'
-  });
+    path: 'folder-closing.json'
+  });}
+  else{
+    var animationIn = bodymovin.loadAnimation({
+      container: loader,
+      renderer: "svg",
+      loop: false,
+      autoplay: false,
+      path: 'ufo-animation.json'
+    });
+  }
 
-  loader1.addEventListener("mouseenter", function() {
-    animationIn.setDirection(-1);
+
+  loader.addEventListener("mouseenter", function() {
+    animationIn.setDirection(1);
     animationIn.play();
   });
 
-  loader1.addEventListener("mouseleave", function() {
-    animationIn.setDirection(1);
+  loader.addEventListener("mouseleave", function() {
+    animationIn.setDirection(-1);
     animationIn.play();
   });
 
 }
 
 
+function crownAnimation(loader) {
+
+if(loader == anim){
+  var animationIn = bodymovin.loadAnimation({
+  container: loader,
+  renderer: "svg",
+  loop: false,
+  autoplay: false,
+  path: 'crown-animation.json'
+});}
+else if(loader == anim4){
+  var animationIn = bodymovin.loadAnimation({
+    container: loader,
+    renderer: "svg",
+    loop: false,
+    autoplay: false,
+    path: 'rocket-animation.json'
+  });}
+  else{
+    var animationIn = bodymovin.loadAnimation({
+      container: loader,
+      renderer: "svg",
+      loop: false,
+      autoplay: false,
+      path: 'pacman-animation.json'
+    });
+  }
+
+
+
+loader.addEventListener("mouseenter", function() {
+  animationIn.setDirection(-1);
+  animationIn.play();
+  animationIn.loop = true;
+});
+
+loader.addEventListener("mouseleave", function() {
+  animationIn.setDirection(1);
+  animationIn.play();
+  animationIn.loop = false;
+});
+
+}
+
+
 loadOpenAnimation(loader1);  
+crownAnimation(anim);  
+loadOpenAnimation(anim2);  
+crownAnimation(anim3);  
+crownAnimation(anim4); 
+
